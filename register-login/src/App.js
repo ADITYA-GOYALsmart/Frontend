ec02ad7183a71ba08e2ec417c247db567368ff78
ADITYA_0093 <aditya.goyal_cs22@gla.ac.in>
2024-07-03 11:02:09 -0700

    two files code LogingRegister with normal css and loginsignup  with tailwind

---
+++ register-login/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Text from './components/Text';
+import LoginSignup from './components/Login/LoginSignup';
+// import LoginSignup from './components/Login/LoginRegister';
+function App() {
+  return (
+    <div>
+      {/* <Text /> */}
+      <LoginSignup/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ register-login/src/components/Text.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Text = ()=> {
+    return(
+        <div className="Text">
+            <h1>Browse Our Delicious Meal</h1>
+        </div>
+    )
+}
+export default Text;
